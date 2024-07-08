5df5b8b4fe3d4451b47dffa019a3532fcb46aa58
Kumarazdevops <169145660+Kumarazdevops@users.noreply.github.com>
2024-07-08 10:14:50 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ docker run -d --name myapp-test myapp:latest npm test
 docker run -d -p 80:3000 --name myapp-prod myapp:latest
 docker run -d -p 3000:3000 -v /path/to/local/dir:/usr/src/app --name app-myapp-dev my
 app:latest
-docker network create myapp-network
+docker network create network-myapp
 docker run -d --network network-myapp --name dv-myapp myapp:latest
 docker run -d --network network-myapp --name dv-myapp-db myapp:latest
